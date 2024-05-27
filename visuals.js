d1ff135b5f76d19b48ebf316517a8f25bb9c4005
ANDYMAN <57153199+andyman087@users.noreply.github.com>
2024-05-27 17:34:36 +0100

    Update visuals.js

--- visuals.js
+++ visuals.js
@@ -52,7 +52,7 @@ function createAchievementsPopup(mappedResults, totalValue) {
                             <img src="${imageUrl}" id="achievement-img-${achievement.rank}-${subCategory.subCategory}" alt="${achievement.rank}" class="achievement-image">
                             <div class="achievement-description">${achievement.description}</div>
                             <div class="achievement-tooltip">${criteriaList}</div>
-                            <div class="achievement-progress">Progress: ${achievement.highlightValue} / ${achievement.criteria[achievement.highlight].min || achievement.count}</div>
+                            <div class="achievement-progress">Progress: ${achievement.highlightValue} / ${(achievement.criteria[achievement.highlight] && achievement.criteria[achievement.highlight].min) || achievement.count}</div>
                         </div>`;
             }).join('');
             return `<div>
